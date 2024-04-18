cfce282dc4ee75dc579bcd9de4dff5dadfa45ab8
Savio Rosa <saviosodre6@gmail.com>
2024-04-18 10:40:20 -0300

    terceiro commit

--- js/index.js
+++ js/index.js
@@ -1,11 +1,55 @@
-function insert(num) {
-    let mostra = document.getElementById('mostra');
-    let numero = mostra.textContent.trim(); // Remova os espaços em branco do início e do fim
+function clearDisplay() {
 
-    // Verifique se o conteúdo atual é apenas "Calcule!"
-    if (numero === "Calcule !") {
-        mostra.innerHTML = num;
-    } else {
-        mostra.innerHTML += num;
+    
+    let mostra = document.getElementById('mostra')
+
+
+        mostra.innerHTML = 'Calcule !'
+    
+    
+}
+function back() {
+    let mostra = document.getElementById('mostra')
+    let conteudo = mostra.innerHTML;
+    mostra.innerHTML = conteudo.slice(0, -1);
+}
+
+function insert(num) {
+
+        let mostra = document.getElementById('mostra')
+        let numero = mostra.textContent.trim()
+
+        if (numero.length >= 16) {
+            return; 
+        }
+
+        
+        if (numero === "Calcule !" ) {
+            mostra.innerHTML = num
+        } else {
+            mostra.innerHTML += num
+        }
+
+
+
+
+}
+
+function calcula() {
+    let mostra = document.getElementById('mostra');
+    let conteudo = mostra.innerHTML;
+
+    if (conteudo) {
+        let resultado = eval(conteudo);
+        if (!isNaN(resultado) && Number.isFinite(resultado)) {
+            if (Number.isInteger(resultado)) {
+                mostra.innerHTML = resultado;
+            } else {
+                mostra.innerHTML = resultado.toFixed(10);
+            }
+        } else {
+            mostra.innerHTML = resultado;
+        }
     }
+    
 }
